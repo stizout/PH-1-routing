75493a54017e0cfbbad9f3cdef01451d5af04646
Tyler Collier <tyler.collier+github@devmounta.in>
2018-03-27 20:45:52 -0700

    Use NavLink and BrowserRouter

--- demo/src/index.js
+++ demo/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 // import registerServiceWorker from './registerServiceWorker';
-import { HashRouter as R } from 'react-router-dom';
+import { BrowserRouter as R } from 'react-router-dom';
 
 ReactDOM.render(<R><App /></R>, document.getElementById('root'));
 // registerServiceWorker();
--- demo/src/routes.js
+++ demo/src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Link, Switch } from 'react-router-dom';
+import { Route, Link, Switch, NavLink } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import People from './components/People/People';
@@ -7,11 +7,13 @@ import FAQ from './components/About/FAQ/FAQ';
 import Company from './components/About/Company/Company';
 import Character from './components/Character/Character';
 
+const NotFound = () => <div><h1>Page not found 404</h1></div>
+
 const routes = <div>
   <nav>
-    <Link to="/">Home</Link>
-    <Link to="/about">About</Link>
-    <Link to="/people">People</Link>
+    <NavLink exact activeClassName="big-bold" to="/">Home</NavLink>
+    <NavLink exact activeClassName="big-bold" to="/about">About</NavLink>
+    <NavLink exact activeClassName="big-bold" to="/people">People</NavLink>
   </nav>
   This is App.js
   <Route exact path="/" component={Home} />
